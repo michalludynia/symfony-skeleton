385261f47dea0da03e0b7eb4170934457fcb1229
Michal Ludynia <michal.ludynia@transfergo.com>
2023-09-02 13:27:28 +0200

    Added phpunit

---
+++ tests/HealthcheckTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class HealthcheckTest extends TestCase
+{
+    public function testHealth(): void {
+        $this->assertTrue(true);
+    }
+}
